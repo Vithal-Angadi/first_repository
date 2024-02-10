8b04c9ea828e24de299ff930f2dd0dba6e3ca5dc
vithal <vithalmangaadi@gmail.com>
2024-02-11 01:06:47 +0530

    updated

--- script_examples.sh
+++ script_examples.sh
@@ -1,6 +1,43 @@
 #!/bin/bash
 
+#Ex:1
 a=10
 b="vithal"
 echo $a $b
+
+#Ex:2
+
+a=10
+b="vithal"
+b=20
+
+echo $a $b 
+
+#Ex:3
+
+
+a=10
+echo "$a" #double quote-command-execute
+echo '$a' #single-quote-string-print
+
+#Ex:4
+
+first=`ls` #back-tick-command-execute
+seccond="$(ls -lrt)"
+
+#Ex:5-Debug_Enable[-x],Debug_disable[+x]
+
+set -x
+
+a=10
+b="$(ls -lrt)"
+
+echo $a
+echo $b
+
+set +x  
+
+#Ex:6
+
+
 
